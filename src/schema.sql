94f14c6ef063cbd794e148c5ab1085a0ff07fd86
Jon Craton <jncraton@gmail.com>
2017-10-30 19:28:36 -0400

    Schema updates

--- src/schema.sql
+++ src/schema.sql
@@ -1,35 +1,46 @@
 .timer on
 
-create table if not exists transactions (
+create table if not exists set_transactions (
   id integer primary key,
   date timestamp default current_timestamp,
   set_num varchar(16),
+  quantity smallint,
+  notes varchar(255),
+  foreign key (set_num) references sets(set_num)
+);
+
+create table if not exists part_transactions (
+  id integer primary key,
+  date timestamp default current_timestamp,
   part_num varchar(16),
   color_id smallint,
   quantity smallint,
-  lot varchar(32),
+  from_set_num varchar(16),
   notes varchar(255),
-  foreign key (set_num) references sets(set_num),
   foreign key (part_num) references parts(part_num),
-  foreign key (color_id) references parts(color_id)
+  foreign key (color_id) references parts(color_id),
+  foreign key (from_set_num) references sets(set_num)
 );
 
+drop view if exists my_set_parts;
+create view if not exists my_set_parts
+as
+select 
+  part_num,
+  color_id,
+  sum(set_parts.quantity * set_transactions.quantity) as quantity
+from set_transactions
+left outer join set_parts on
+  set_parts.set_num = set_transactions.set_num
+group by part_num, color_id;
+
+drop view if exists my_parts;
 create view if not exists my_parts
 as
-select p as part_num, c as color_id, sum(q) as quantity from (
-  select
-    part_num as p,
-    color_id as c,
-    quantity as q
-  from transactions
-  where transactions.set_num is null
-  union
-  select 
-    set_parts.part_num as p,
-    set_parts.color_id as c,
-    set_parts.quantity * transactions.quantity as q
-  from transactions
-  left outer join set_parts on
-    set_parts.set_num = transactions.set_num
-  where transactions.set_num not null
-) group by part_num, color_id;
+select part_num, color_id, sum(quantity) as quantity from (
+  select part_num, color_id, quantity
+  from part_transactions
+  union all
+  select part_num, color_id, quantity
+  from my_set_parts
+);
