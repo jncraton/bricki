9062a30b213b95216969c842348eb30332785eca
Jon Craton <jncraton@gmail.com>
2017-12-24 16:08:01 -0500

    Added initial CLI implementation

---
+++ bricki/cli.py
@@ -0,0 +1,29 @@
+import helpers
+
+if __name__ == '__main__':
+  last_part = None
+  last_color = None
+
+  while(True):
+    command = input('Enter as quantity, color, part:')
+    if command[0:4] == 'exit':
+      exit(0)
+
+    try:
+      (quantity, color, part) = command.split(',')
+    except ValueError:
+      print('Needs all three parameters')
+      quantity = None
+
+    if quantity:
+      if not part:
+        part = last_part_name
+  
+      if not color:
+        color = last_color_name
+  
+      color, last_color_name = helpers.search_color(color)[0]
+  
+      part, last_part_name = helpers.search_part(part)[0]
+  
+      print(quantity, color, part)
