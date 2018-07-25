e161b7d3c0b22b54c1aa4dfb3ce7d9f30913b778
Jon Craton <jncraton@gmail.com>
2018-07-25 09:38:56 -0400

    Range to prior 60 days

--- scripts/dumprecentparts.sql
+++ scripts/dumprecentparts.sql
@@ -11,6 +11,6 @@ left outer join colors on
   colors.id = part_transactions.color_id
 left outer join canonical_parts on
   canonical_parts.part_num = part_transactions.part_num
-where date > datetime('now', '-14 days')
+where date > datetime('now', '-60 days')
 group by canonical_parts.part_num, colors.id
 having sum(quantity) > 0;
