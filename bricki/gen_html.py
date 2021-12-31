348174cda3cf3fe5dec693efe6a438685b82517d
Jon Craton <jncraton@gmail.com>
2021-12-30 22:51:21 -0500

    Combine mold variations

--- bricki/gen_html.py
+++ bricki/gen_html.py
@@ -143,15 +143,16 @@ with open(path + "elements.html", "w") as out:
     parts.name,
     sum(quantity),
     colors.id,
-    parts.part_num,
+    canonical_part_num,
     part_bins.bin_id,
     element_bins.bin_id
   from my_parts
   join parts on parts.part_num=my_parts.part_num 
   join colors on colors.id=my_parts.color_id
+  join canonical_parts on canonical_parts.part_num = my_parts.part_num
   left join part_bins as element_bins on my_parts.part_num=element_bins.part_num and my_parts.color_id=element_bins.color_id
   left join part_bins on my_parts.part_num=part_bins.part_num and part_bins.color_id=-1
-  group by parts.part_num, colors.id
+  group by canonical_part_num, colors.id
   having sum(quantity) > 0
   order by sum(quantity) desc
   """
