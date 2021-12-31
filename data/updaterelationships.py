96296403d4fe20aee2dd32a002ba201293cc07f3
Jon Craton <jncraton@gmail.com>
2021-12-30 23:13:42 -0500

    Remove intermediate files

--- data/updaterelationships.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open('stored-together.csv') as f:
-    with open('part_relationships.csv', 'w') as out:
-        for row in csv.reader(f):
-            parent = row[1]
-
-            for child in row[2:]:
-                out.write(f"M,{child},{parent}\n")
