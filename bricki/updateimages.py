cfc467d221e8f05ddbb5d84f9e98258ee6f16e8d
Jon Craton <jncraton@gmail.com>
2022-01-09 17:27:20 -0500

    Add local part images for sorted parts

---
+++ bricki/updateimages.py
@@ -0,0 +1,31 @@
+import helpers
+import urllib.request 
+
+parts = helpers.query(
+    """
+      select 
+        parts.name,
+        canonical_part_num,
+        sum(quantity) as quantity,
+        part_bins.bin_id,
+        min(my_parts.color_id),
+        count(distinct element_bins.color_id)
+      from my_parts
+      join canonical_parts on canonical_parts.part_num = my_parts.part_num
+      join parts on parts.part_num=canonical_part_num
+      join part_bins on canonical_part_num=part_bins.part_num and part_bins.color_id=-1
+      left join part_bins as element_bins on canonical_part_num=element_bins.part_num and element_bins.color_id=my_parts.color_id
+      where part_bins.bin_id not null
+      group by canonical_part_num
+      order by part_bins.bin_id, parts.name asc
+      """
+)
+
+print(f"Downloading {len(parts)} images...")
+
+for p in parts:
+    img = f'https://cdn.rebrickable.com/media/thumbs/parts/ldraw/{p[4] or 71}/{p[1]}.png/250x250p.png'
+    try:
+        urllib.request.urlretrieve(img, f'www/images/{p[1]}.png')
+    except:
+        print(f'Error with {p[1]} {p[0]}')
