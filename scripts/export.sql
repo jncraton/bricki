a4b77b71b587e8a68ba3cb24be508fb6bd3d23fd
Jon Craton <jncraton@gmail.com>
2022-01-13 21:59:14 -0500

    Prefer null to empty string when exporting

--- scripts/export.sql
+++ scripts/export.sql
@@ -7,5 +7,6 @@ select * from set_transactions;
 .output data/part_transactions.csv
 select * from part_transactions;
 
+update part_bins set section_id=null where section_id='';
 .output data/part_bins.csv
 select * from part_bins where bin_id != '' order by bin_id, section_id, part_num;
