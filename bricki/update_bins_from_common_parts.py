2640d935a2fe5d56316dc2ac30be316bf44cb318
Jon Craton <jncraton@gmail.com>
2022-04-03 21:01:31 -0400

    Remove old script for bulk bin editing

--- bricki/update_bins_from_common_parts.py
+++
@@ -1,7 +0,0 @@
-import csv
-import helpers
-
-with open('dumps/common-parts.csv') as f:
-    for b in csv.DictReader(f):
-        helpers.query('insert or ignore into part_bins (part_num, bin_id) values (?,?)', (b['part_num'], b['bin_id']))
-        helpers.query('update part_bins set bin_id = ? where part_num = ?', (b['bin_id'], b['part_num']))
