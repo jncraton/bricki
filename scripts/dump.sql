a14244213d5a839795f974199891f9973f73ef3b
Jon Craton <jncraton@gmail.com>
2023-01-12 12:23:19 -0500

    Dump printed parts

--- scripts/dump.sql
+++ scripts/dump.sql
@@ -69,6 +69,31 @@ left outer join canonical_parts on
 group by canonical_part_num, colors.id
 order by colors.name, parts.name;
 
+.output dumps/printed-parts.csv
+select colors.name, parts.name, sum(quantity), my_parts.part_num, color_id, canonical_part_num
+from my_parts
+left outer join parts on
+  parts.part_num = my_parts.part_num
+left outer join colors on
+  colors.id = my_parts.color_id
+left outer join canonical_parts on
+  canonical_parts.part_num = my_parts.part_num
+where
+  my_parts.part_num like '%pr%' and
+  parts.name like '%print%' and
+  parts.name not like '%minifig%' and
+  parts.name not like '%mario%' and
+  parts.name not like '%nintendo%' and
+  parts.name not like '%figure%' and
+  parts.name not like '%instrument%' and
+  parts.name not like '%equipment%' and
+  parts.name not like '%sail%' and
+  parts.name not like '%baseplate%' and
+  parts.name not like '%animal%'
+group by canonical_part_num, colors.id
+having sum(quantity) > 0
+order by parts.part_num, colors.name;
+
 .output dumps/common-elements.csv
 select sum(quantity) as quantity, colors.name, parts.name, canonical_part_num
 from my_parts
