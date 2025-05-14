0d71eb85801a2fc4ca37e343e0b8c7c5f9c71293
Jon Craton <jncraton@gmail.com>
2025-05-14 13:55:52 -0400

    Adjust filter

--- bricki/colorize-gray-to-red.sh
+++ bricki/colorize-gray-to-red.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-convert colortest.png -channel G -evaluate set 0 -channel B -evaluate set 0 -channel R -evaluate multiply 1.5 colortestred.png
+convert colortest.png -channel G -evaluate set 0 -channel B -evaluate set 0 -channel R -evaluate multiply 1.0 colortestred.png
